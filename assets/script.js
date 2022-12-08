6086b6a7bacb059034fb2910467f6314420f7ef4
Andy Kleindienst <andy.kleindienst@gmail.com>
2022-12-07 22:28:22 -0500

    changed the code so the password can be written every time the button is pushed

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,19 @@ let numberChars = "1234567890"
 let specialChars = "!@#$%^&*"
 let charString = "";
 
-function password() {
+function newPassword() {
   let numCharacters = prompt("How many characters would you like your password to be? (Between 8 and 128 characters")
   if (numCharacters >= 8 && numCharacters <= 128) {
     confirm("Your password is: " + numCharacters + " characters long")
   } else if (numCharacters < 8) {
     alert("You password is too SHORT. Must be between 8 and 15 characters")
-    return password()
+    return newPassword()
   } else {
     alert("Your password is too LONG. Must be between 8 and 15 characters")
+    return newPassword()
   }
   // Ask the user if they want lowercase letters
-  let lowercase = confirm("Would you like to use lowercase letters? Y or N");
+  let lowercase = confirm("Would you like to use lowercase letters?");
   if (lowercase) {
     alert("Your password will include lowercase letters")
     charString = charString.concat(lowerChars);
@@ -36,8 +37,8 @@ function password() {
     alert("Your password will NOT contain UPPERCASE letters")
   }
   // ask the user if they want numbers
-  let numbers = confirm("would you like to use numbers?");
-  if (numbers === "Y") {
+  let numbers = confirm("Would you like to use numbers?");
+  if (numbers) {
     alert("Your password will include numbers")
     charString = charString.concat(numberChars)
     console.log(charString)
@@ -54,7 +55,7 @@ function password() {
     alert("Your password will NOT contain special characters")
   }
 
-  // Determine the 
+  // Actually make the password
   function makePassword() {
     let password = ""
     for (let i = 0; i < numCharacters; i++) {
@@ -66,13 +67,14 @@ function password() {
     return password
   }
   return makePassword()
-  // let newPassword = makePassword()
 }
 
 var generateBtn = document.querySelector("#generate");
-var password = password();
+// !!!!THIS CODE NEEDS TO BE IN THE FUNCTION WRITE PASSWORD so it can be run multiple times!!!!
+
 // Write password to the #password input
 function writePassword() {
+  var password = newPassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
